bed105de1f379d9231d49d4c96419c195d5ba537
narcis <narcis_alexinho@yahoo.com>
2022-03-22 10:49:16 +0200

    Add grid and blocks

---
+++ index.js
@@ -0,0 +1,7 @@
+const grid =document.querySelector('.grid')
+
+const block=document.createElement('div')
+block.classList('block')
+grid.style.left='100px'
+grid.style.bottom='50px'
+grid.appendChild(block)
