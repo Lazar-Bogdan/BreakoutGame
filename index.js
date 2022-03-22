6194eb1a545516b7676ac4a0b852adf6b99232d6
narcis <narcis_alexinho@yahoo.com>
2022-03-22 11:19:53 +0200

    More bug fixes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const grid =document.querySelector('.grid')
 
+//draw a block
+function addBlock(){
 const block=document.createElement('div')
 block.classList('block')
 grid.style.left='100px'
 grid.style.bottom='50px'
 grid.appendChild(block)
+}
+
